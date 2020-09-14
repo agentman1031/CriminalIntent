ce392e66297e956f96038b8cc107e69ca125d5dd
Agentman1031 <Agentman1031@gmail.com>
2020-09-13 20:43:20 -0500

    Added @entity

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,11 +1,12 @@
 package com.example.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 import java.util.Date //TODO: Check if I need this if I have the above.
 
-data class Crime(
-    val id: UUID = UUID.randomUUID(),
-    var title: String = "",
-    var date: Date = Date(),
-    var isSolved: Boolean = false
-)
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false)
